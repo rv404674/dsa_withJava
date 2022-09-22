e011362ee25ca3f733d74a745c941f2825e63aeb
Rahul Verma <rv404674@gmail.com>
2022-09-22 14:56:34 +0530

    Solved merge intervals

--- leetcode/src/array/MergeIntervals.java
+++ leetcode/src/array/MergeIntervals.java
@@ -12,38 +12,66 @@ import java.util.List;
 
 public class MergeIntervals {
     class Solution {
-        public int[][] merge(int[][] intervals) {
-            if(intervals.length <=1)
+
+        /**
+         * Time - O(nlogn) + O(n*k)
+         * space - O(n)
+         * Wrote Myself - Compared one by one.
+         */
+        public int[][] mergeAppr1(int[][] intervals) {
+            // use your own approach
+            if(intervals.length <= 1)
                 return intervals;
 
+            Arrays.sort(intervals, (o1,o2) -> o1[0]-o2[0]);
+
             List<int[]> result = new ArrayList<>();
-            Arrays.sort(intervals, (arr1,arr2) -> Integer.compare(arr1[0], arr2[0]));
+            result.add(new int[]{intervals[0][0], intervals[0][1]});
+
+            for (int i = 1; i< intervals.length; i++){
+                int[] prevInterval = result.get(result.size() - 1);
+                int[] curInterval = intervals[i];
+
+                if (prevInterval[1] >= curInterval[0]){
+                    result.set(result.size() - 1, new int[]{prevInterval[0], Math.max(curInterval[1], prevInterval[1])});
+                } else {
+                    result.add(new int[]{curInterval[0], curInterval[1]});
+                }
+            }
+
+            return result.toArray(new int[result.size()][]);
+        }
+
+        /**
+         * The diff b/w above one and this approach is that, here we are checking one by one.
+         * Just process until you find an invalid case.
+         *
+         * also because of this approach, we dont need to update the original array.
+         */
+        public int[][] merge(int[][] intervals) {
+            if (intervals.length <=1)
+                return intervals;
+
+            Arrays.sort(intervals, (((o1, o2) -> o1[0] - o2[0])));
+            List<int[]> results = new ArrayList<>();
 
             int start = intervals[0][0];
             int end = intervals[0][1];
-            for(int[] i:intervals){
-                if(i[0] <= end)
+
+            for (int[] i: intervals) {
+                if(end >= i[0]) {
                     end = Math.max(end, i[1]);
-                else{
-                    result.add(new int[]{start, end});
+                } else {
+                    // add to the result now.
+                    results.add(new int[]{start, end});
                     start = i[0];
                     end = i[1];
                 }
             }
 
-            result.add(new int[]{start, end});
-            // NOTE:
-            // size() vs length
-            // size() - will give you the total elements in a collection. Size is applicable on collections.
-            // eg
-            // int[] x = new int[5];
-            // x[0] = 1;
-            // here size=1, whereas lenght = 5
-            // for list
-            // [ [1,2], [3,4] ]
-            // size = 2.
-            // NOTE: creating a 2d array here.
-            return result.toArray(new int[result.size()][]);
+            results.add(new int[]{start, end});
+            return results.toArray(new int[results.size()][]);
         }
+
     }
 }
