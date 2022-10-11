41a28c52ef97b61e3e11aca512e62a3c3ecab9e5
Rahul Verma <rv404674@gmail.com>
2022-10-11 15:02:56 +0530

    solved some graph questions

--- leetcode/src/graphs/dfsPractice/Dfs.java
+++ leetcode/src/graphs/dfsPractice/Dfs.java
@@ -6,58 +6,73 @@ import java.util.LinkedList;
 /*
 NOTE: we need to change the implementation to a HashMap, if we get alphabetical nodes.
  */
-class Graph{
+class Graph {
     private int V;
-    private LinkedList<Integer>[] adjancyList;
+    // an array having value as LinkedList<Integer> for each index.
+    // int[] array = new int[3];
+    private LinkedList<Integer>[] adjacenyList;
 
-    public Graph(int v){
-        V = v;
-        adjancyList = new LinkedList[V];
-        for (int i=0; i<v; i++){
-            adjancyList[i] = new LinkedList<Integer>();
-        }
+    Graph(int v) {
+        this.V = v;
+        this.adjacenyList = new LinkedList[v];
+        for (int i=0; i<v; i++)
+            adjacenyList[i] = new LinkedList<>();
     }
 
-    void addEdge(int v, int w){
-        adjancyList[v].add(w);
+    void addEdge(int x, int y){
+        this.adjacenyList[x].add(y);
     }
 
     void printGraph(){
-        for(int i=0; i<V; i++)
-            System.out.println("Edge from " + i + " " + adjancyList[i]);
+        System.out.println("Printing graph");
+        for(int i=0; i<V;i++){
+            System.out.println("Edges from: " + i + adjacenyList[i]);
+        }
     }
 
-    void start_dfs(){
-        // false by default in java
-        boolean[] visited = new boolean[V];
-        dfs_util(0, visited);
+    void dfs(){
+        boolean[] visited = new boolean[this.V];
+        dfsUtil(0, visited);
     }
 
-    void dfs_util(int node, boolean[] visited){
+    void dfsUtil(Integer node, boolean[] visited){
         System.out.println(node);
         visited[node] = true;
 
-        Iterator<Integer> i = adjancyList[node].iterator();
-        while(i.hasNext()){
-            int current = i.next();
-            if(!visited[current])
-                dfs_util(current, visited);
+        Iterator<Integer> neighbours = adjacenyList[node].iterator();
+        while(neighbours.hasNext()){
+            int neighbour = neighbours.next();
+            if(!visited[neighbour]) {
+                dfsUtil(neighbour, visited);
+            }
         }
+//        for(int neighbour : adjacenyList[node]){
+//            if(!visited[neighbour]){
+//                dfsUtil(node, visited);
+//            }
+//        }
     }
 }
+
 
 public class Dfs {
     public static void main(String[] args){
         Graph graph = new Graph(6);
+
         graph.addEdge(0,1);
         graph.addEdge(0,2);
-        graph.addEdge(1,3);
-        graph.addEdge(2,3);
-        graph.addEdge(3,4);
-        graph.addEdge(3,5);
+        graph.addEdge(1, 0);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 0);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 1);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 5);
+        graph.addEdge(4, 3);
+        graph.addEdge(5, 3);
 
-        graph.start_dfs();
-
-
+        graph.printGraph();
+        System.out.println("DFS Started");
+        graph.dfs();
     }
 }
---
+++ leetcode/src/graphs/dfsPractice/NestedListWeightSUm.java
@@ -0,0 +1,43 @@
+package graphs.dfsPractice;
+
+import java.util.List;
+
+interface NestedInteger {
+
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // Set this NestedInteger to hold a single integer.
+    public void setInteger(int value);
+
+    // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+    public void add(NestedInteger ni);
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return empty list if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+}
+
+public class NestedListWeightSUm {
+
+    public int depthSum(List<NestedInteger> nestedList) {
+        return dfs(nestedList, 1);
+    }
+
+    public int dfs(List<NestedInteger> nestedList, int level){
+        int result = 0;
+        for(NestedInteger ni: nestedList){
+            if(ni.isInteger()){
+                result += ni.getInteger() * level;
+            } else {
+                // nested list
+                result += result + dfs(ni.getList(), level+1);
+            }
+        }
+        return result;
+    }
+}
