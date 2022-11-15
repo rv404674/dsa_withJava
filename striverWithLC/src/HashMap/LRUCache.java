5bd366db48b696c062423c0d196b4dda5cbb1769
Rahul Verma <rv404674@gmail.com>
2022-11-15 15:56:07 +0530

    wrote lru cache in a separate way

--- striverWithLC/src/HashMap/LRUCache.java
+++ striverWithLC/src/HashMap/LRUCache.java
@@ -13,31 +13,34 @@ class Node {
     Node next, prev;
     int key, val;
 
-    public Node(int key, int val){
+    public Node(int key, int val) {
         this.key = key;
         this.val = val;
     }
 }
 
+// Double linked list + Hashmap
+// NOTE: newest after head
+// oldest will be the tail Node
 class LRUCache {
-    Node head = new Node(0,0);
-    Node tail = new Node(0,0);
+    Node head = new Node(0, 0);
+    Node tail = new Node(0, 0);
 
     int capacity;
-    HashMap<Integer, Node> hashMap = new HashMap<>();
+    HashMap<Integer, Node> hashMap;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
         head.next = tail;
         tail.prev = head;
+        hashMap = new HashMap<>();
     }
 
     public int get(int key) {
-        if(hashMap.containsKey(key)) {
+        if (hashMap.containsKey(key)) {
             Node node = hashMap.get(key);
-            remove(node);
-            add(node);
-
+            removeNode(node);
+            addNode(node);
             return node.val;
         }
 
@@ -45,33 +48,35 @@ class LRUCache {
     }
 
     public void put(int key, int value) {
-        // if the key is already present
-        if(hashMap.containsKey(key))
-            remove(hashMap.get(key));
-        // cache is full
-        else if (hashMap.size() == capacity)
-            remove(tail.prev);
+        // if the key already exists
+        if (hashMap.containsKey(key))
+            removeNode(hashMap.get(key));
+        else if (hashMap.size() == this.capacity)
+            // hasmap is full. delete the oldes i.e last node
+            removeNode(this.tail.prev);
 
-        add(new Node(key, value));
+        addNode(new Node(key, value));
     }
 
-    // Insert just after head
-    public void add(Node node){
+    // add it to the hashmap
+    // add it after Head.
+    void addNode(Node node) {
         hashMap.put(node.key, node);
-        Node headNext = head.next;
-        // first fix head side
-        head.next = node;
+        // add after the head
+        node.next = head.next;
+        head.next.prev = node;
+
         node.prev = head;
-        headNext.prev = node;
-        node.next = headNext;
+        head.next = node;
+
     }
 
-    // remove the node
-    public void remove(Node node) {
-        hashMap.remove(node.key);
+    void removeNode(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
+        hashMap.remove(node.key);
     }
+
 
     public static void main(String[] args) {
         LRUCache lRUCache = new LRUCache(2);
